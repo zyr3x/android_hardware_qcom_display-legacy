f94324637fe40f18436a292dc0373e6cf6093512
Darescu Ionut <johnny@esi.ro>
2013-04-27 11:37:42 +0300

    include kernel headers during build

--- libgralloc/Android.mk
+++ libgralloc/Android.mk
@@ -20,7 +20,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := gralloc.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes)
+LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libmemalloc libgenlock
 LOCAL_SHARED_LIBRARIES        += libqdutils libGLESv1_CM
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"gralloc\"
@@ -31,7 +31,7 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE           := libmemalloc
 LOCAL_MODULE_TAGS      := optional
-LOCAL_C_INCLUDES       := $(common_includes)
+LOCAL_C_INCLUDES       := $(common_includes) $(kernel_includes)
 LOCAL_SHARED_LIBRARIES := $(common_libs) libgenlock libqdutils
 LOCAL_CFLAGS           := $(common_flags) -DLOG_TAG=\"memalloc\"
 LOCAL_SRC_FILES        := alloc_controller.cpp
