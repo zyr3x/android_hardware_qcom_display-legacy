ef45ce32f2f4598eee783d7bec7f0c4a84ce44bf
Dorian Snyder <dastin1015@gmail.com>
2013-04-27 00:56:05 -0700

    display: Use actual kernel headers for all builds

--- common.mk
+++ common.mk
@@ -32,3 +32,8 @@ endif
 ifeq ($(TARGET_USES_QCOM_BSP),true)
     common_flags += -DQCOM_BSP
 endif
+
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+ endif
