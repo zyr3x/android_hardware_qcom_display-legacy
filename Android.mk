78b4edf3bb4591f2fe68ba147245116e52f8b45f
zyr3x <izzzum@gmail.com>
2013-11-20 09:34:36 +0300

    Fixed makefile

--- Android.mk
+++ Android.mk
@@ -3,12 +3,6 @@ display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils
 display-hals += libmemtrack
 
-ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf)
-
-display-hals := libgralloc libgenlock libcopybit libvirtual
-display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
-display-hals += libmemtrack
-
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
 endif
