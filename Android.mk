7c74db0d99c280a3f63267d20f26dc4ea233bfb0
zyr3x <izzzum@gmail.com>
2013-11-30 13:24:16 +0300

    Initial memory tracker HAL
    Squashed commit of the following, from AOSP display:
    libmemtrack: Add libmemtrack to HAL directory listing
    
    Change-Id: Ifbe55a339d589ad1bfab07fef684a738976877bd
    
    commit 454565c66a5d068c0722aa3bb1fccc4c383a5808
    Author: Colin Cross <ccross@android.com>
    Date:   Mon Sep 30 13:09:02 2013 -0700
    
        fix fd leak in memtrack.msm8960
    
        Close smaps_fd when exiting function.
    
        Bug: 11007903
        Change-Id: I4a9fcd1e84aae195d229e5dd11f99ccb9f6f5006
    
    commit 28c93c950cd82667a43dd0dd64043274eb7a1e55
    Author: Colin Cross <ccross@android.com>
    Date:   Mon Sep 16 15:16:08 2013 -0700
    
        Fix remainig exynos references
    
        Also fix a compile warning by replacing enum with int in the getMemory
        function.
    
        Change-Id: Ic4bbba425486bb7f456e928968afa5f2b04ca7ea
    
    commit b46313b2e4d0e7fd1a475bf26ce396bcd0243921
    Author: Colin Cross <ccross@android.com>
    Date:   Fri Sep 6 18:03:02 2013 -0700
    
        Initial msm8x64 memory tracker HAL
    
        Reads memory info from kgsl to track unaccounted for GL and ION
        memory.  Right now it has to parse every allocation and then
        cross-reference it against /proc/pid/smaps.  It would be faster
        to modify the kgsl driver to keep counters that could just be
        read directly.
    
        Bug: 10294768
        Change-Id: Icb17558f03ce53bc594fbee097e51fe82e15dc31
    
    Change-Id: I7816cf5f506ac94229646013683bdba674110ede
    
    Conflicts:
    	Android.mk (reverted from commit 470e0596ae3547e630c90cdb73438586798ceeb4)

--- Android.mk
+++ Android.mk
@@ -1,8 +1,6 @@
 ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils
-display-hals += libmemtrack
-
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
 endif
